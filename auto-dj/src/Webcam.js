cae1c02259169f1d9c86fd1fb286f01f408abf24
Andrew Masek <amasek@purdue.edu>
2020-10-17 17:41:06 -0400

    screenshot of webcam is converted to base64 string every 60 seconds

--- auto-dj/src/Webcam.js
+++ auto-dj/src/Webcam.js
@@ -5,6 +5,7 @@ class Video extends React.Component {
         super(props);
         this.streamCamVideo = this.streamCamVideo.bind(this)
     }
+
     streamCamVideo() {
         // DEAF DJ!
         var constraints = { audio: false, video: { width: 1280, height: 720 } };
@@ -22,16 +23,36 @@ class Video extends React.Component {
                 console.log(err.name + ": " + err.message);
             }); // always check for errors at the end.
     }
+
+    captureImage() {
+        var canvas = document.getElementById('canvas');
+        var video = document.querySelector('video');
+
+        canvas.width = 1280;
+        canvas.height = 720;
+        canvas.getContext('2d').drawImage(video, 0, 0, 1280, 720);
+
+        document.getElementById("printresult").innerHTML = canvas.toDataURL();
+        console.log(canvas.toDataURL());
+    }
+
     render() {
-        return (
-            <div>
-                <div id="container">
-                    <video autoPlay={true} id="videoElement" controls></video>
-                </div>
-                <br />
-                <button onClick={this.streamCamVideo}>Start streaming</button>
+
+        const videoContainer = <div>
+            <div id="container">
+                <video autoPlay={true} id="videoElement" controls></video>
             </div>
-        );
+            <br />
+            <button onClick={this.streamCamVideo}>Start streaming</button>
+            {/* <button onClick={this.captureImage}>Take Picture</button> */}
+            <canvas id="canvas"></canvas>
+            <p> Image Converted to String: </p>
+            <p id="printresult"></p>
+        </div>
+
+        setInterval(this.captureImage, 30000);
+
+        return videoContainer;
     }
 }
 
