fff6daad5546df3f9a2527b34f4674d602252bd9
Andrew Masek <amasek@purdue.edu>
2020-10-17 16:32:03 -0400

    fix styling

--- auto-dj/src/App.js
+++ auto-dj/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import CoolTabs from 'react-cool-tabs';
-import './App.css';
 
 class Video extends React.Component {
   render() {
@@ -20,16 +19,24 @@ class Song extends React.Component {
 
 export default class App extends React.Component {
   render() {
+    const mystyle = {
+      margin: 0,
+      width: "700",
+      height: 900,
+      border: "3px solid green",
+      overflow: 'hidden',
+      background: 'white'
+    };
     return (
       <div>
         <CoolTabs
           tabKey={'1'}
-          style={{ margin: 0, height: 400, background: 'white' }}
-          activeTabStyle={{ background: 'red', color: 'white' }}
-          unActiveTabStyle={{ background: 'green', color: 'black' }}
-          activeLeftTabBorderBottomStyle={{ background: 'blue', height: 4 }}
-          activeRightTabBorderBottomStyle={{ background: 'yellow', height: 4 }}
-          tabsBorderBottomStyle={{ background: 'orange', height: 4 }}
+          style={mystyle}
+          activeTabStyle={{ background: '#3f8a53', color: 'white' }}
+          unActiveTabStyle={{ background: '#90f5ab', color: 'black' }}
+          activeLeftTabBorderBottomStyle={{ background: '#000000', height: 4 }}
+          activeRightTabBorderBottomStyle={{ background: '#000000', height: 4 }}
+          tabsBorderBottomStyle={{ background: '#efff3b', height: 4 }}
           leftContentStyle={{ background: 'lightgreen' }}
           rightContentStyle={{ background: 'lightblue' }}
           leftTabTitle={'Webcam View'}
